60ba6f4871e37fc808a952d21914c81a60edce59
MagiaSN <MagiaSN@yeah.net>
2017-01-23 13:27:44 +0800

    Improve z-move input (#101)

--- js/damage.js
+++ js/damage.js
@@ -98,27 +98,35 @@ function getDamageResult(attacker, defender, move, field) {
     } else if (move.name === "Nature Power") {
         move.type = field.terrain === "Electric" ? "Electric" : field.terrain === "Grassy" ? "Grass" : field.terrain === "Misty" ? "Fairy" : field.terrain === "Psychic" ? "Psychic" : "Normal";
     }
-    
-    var isAerilate = attacker.ability === "Aerilate" && move.type === "Normal";
-    var isPixilate = attacker.ability === "Pixilate" && move.type === "Normal";
-    var isRefrigerate = attacker.ability === "Refrigerate" && move.type === "Normal";
-    var isGalvanize = attacker.ability === "Galvanize" && move.type === "Normal";
-    var isLiquidVoice = attacker.ability === "Liquid Voice" && move.isSound;
-    if (isAerilate) {
-        move.type = "Flying";
-    } else if (isGalvanize) {
-        move.type = "Electric";
-    } else if (isLiquidVoice) {
-        move.type = "Water";
-    } else if (isPixilate) {
-        move.type = "Fairy";
-    } else if (isRefrigerate) {
-        move.type = "Ice";
-    } else if (attacker.ability === "Normalize") {
-        move.type = "Normal";
-        description.attackerAbility = attacker.ability;
+
+    var isAerilate = false;
+    var isPixilate = false;
+    var isRefrigerate = false;
+    var isGalvanize = false;
+    var isLiquidVoice = false;
+
+    if (!move.isZ) {
+        isAerilate = attacker.ability === "Aerilate" && move.type === "Normal";
+        isPixilate = attacker.ability === "Pixilate" && move.type === "Normal";
+        isRefrigerate = attacker.ability === "Refrigerate" && move.type === "Normal";
+        isGalvanize = attacker.ability === "Galvanize" && move.type === "Normal";
+        isLiquidVoice = attacker.ability === "Liquid Voice" && move.isSound;
+        if (isAerilate) {
+            move.type = "Flying";
+        } else if (isGalvanize) {
+            move.type = "Electric";
+        } else if (isLiquidVoice) {
+            move.type = "Water";
+        } else if (isPixilate) {
+            move.type = "Fairy";
+        } else if (isRefrigerate) {
+            move.type = "Ice";
+        } else if (attacker.ability === "Normalize") {
+            move.type = "Normal";
+            description.attackerAbility = attacker.ability;
+        }
     }
-    
+
     var typeEffect1 = getMoveEffectiveness(move, defender.type1, attacker.ability === "Scrappy" || field.isForesight, field.isGravity);
     var typeEffect2 = defender.type2 ? getMoveEffectiveness(move, defender.type2, attacker.ability === "Scrappy" || field.isForesight, field.isGravity) : 1;
     var typeEffectiveness = typeEffect1 * typeEffect2;
@@ -335,6 +343,11 @@ function getDamageResult(attacker, defender, move, field) {
             (defender.name.indexOf("Genesect") !== -1 && defender.item.indexOf("Drive") !== -1))) {
         bpMods.push(0x1800);
         description.moveBP = move.bp * 1.5;
+    } else if (["Breakneck Blitz","Bloom Doom","Inferno Overdrive","Hydro Vortex","Gigavolt Havoc","Subzero Slammer","Supersonic Skystrike",
+            "Savage Spin-Out","Acid Downpour","Tectonic Rage","Continental Crush","All-Out Pummeling","Shattered Psyche","Never-Ending Nightmare",
+            "Devastating Drake","Black Hole Eclipse","Corkscrew Crash","Twinkle Tackle"].indexOf(move.name) !== -1) {
+        // show z-move power in description
+        description.moveBP = move.bp;
     }
     
     if (field.isHelpingHand) {
