4bc8ea972d0b1dfe4d3ca4b66eba92a3193f6b84
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-12 00:21:27 +0300

    Update index_controls.js

--- js/index_controls.js
+++ js/index_controls.js
@@ -60,11 +60,11 @@ function calculate() {
         maxDamage = result.damage[result.damage.length-1] * p2.moves[i].hits;
         minDisplay = notation === '%' ? Math.floor(minDamage * 1000 / p1.maxHP) / 10 : Math.floor(minDamage * 48 / p1.maxHP);
         maxDisplay = notation === '%' ? Math.floor(maxDamage * 1000 / p1.maxHP) / 10 : Math.floor(maxDamage * 48 / p1.maxHP);
-		if (p2.moves[i].givesHealth) {
+	if (p2.moves[i].givesHealth) {
             var minHealthRecovered = notation === '%' ? Math.floor(minDamage * p2.moves[i].percentHealed * 1000 / p1.maxHP) / 10 : Math.floor(minDamage * p2.moves[i].percentHealed * 48 / p2.maxHP);
             var maxHealthRecovered = notation === '%' ? Math.floor(maxDamage * p2.moves[i].percentHealed * 1000 / p1.maxHP) / 10 : Math.floor(maxDamage * p2.moves[i].percentHealed * 48 / p2.maxHP);
             recoveryText = ' (recovers between ' + minHealthRecovered + notation + ' and ' + maxHealthRecovered + notation + ')';
-		}
+	}
         result.damageText = minDamage + "-" + maxDamage + " (" + minDisplay + " - " + maxDisplay + notation + ")";
         result.koChanceText = p2.moves[i].bp === 0 ? 'nice move'
                 : getKOChanceText(result.damage, p2, p1, field.getSide(0), p2.moves[i].hits, p2.ability === 'Bad Dreams');
