e19ff57acf3514f8370af8ab6e6d8676ecd7796b
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2018-01-06 05:03:18 +0200

    Polyfill Array#indexof

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -1,3 +1,39 @@
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function(searchElement, fromIndex) {
+	var k;
+
+	if (this == null) {
+		throw new TypeError('"this" equals null or n is undefined');
+	}
+
+	var O = Object(this);
+
+	var len = O.length >>> 0;
+
+	if (len === 0) {
+		return -1;
+	}
+
+	var n = +fromIndex || 0;
+
+	if (Math.abs(n) === Infinity) {
+		n = 0;
+	}
+	if (n >= len) {
+		return -1;
+	}
+
+	k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+	while (k < len) {
+		if (k in O && O[k] === searchElement) {
+	    	return k;
+	  	}
+	  	k++;
+	}
+		return -1;
+	};
+}
+
 // input field validation
 var bounds = {
 	"level": [0, 100],
