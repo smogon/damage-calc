336f43c538feb8869a849dae3a9c1dc51d0d2f6f
Austin Couturier <austincouturier@icloud.com>
2019-04-15 11:29:07 -0600

    Fix normalize not boosting normal type moves

--- js/damage.js
+++ js/damage.js
@@ -134,7 +134,7 @@ function getDamageResult(attacker, defender, move, field) {
 		isRefrigerate = attacker.ability === "Refrigerate" && move.type === "Normal";
 		isGalvanize = attacker.ability === "Galvanize" && move.type === "Normal";
 		isLiquidVoice = attacker.ability === "Liquid Voice" && move.isSound;
-		isNormalize = attacker.ability === "Normalize" && move.type !== "Normal";
+		isNormalize = attacker.ability === "Normalize" && move.type;
 		if (isAerilate) {
 			move.type = "Flying";
 		} else if (isGalvanize) {
