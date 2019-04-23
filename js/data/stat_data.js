e8f14ca35bb516e646b70d8a312dae8528cbee82
Austin Couturier <austincouturier@icloud.com>
2019-04-23 13:03:40 -0600

    Add HP to STATS
    fixes #298

--- js/data/stat_data.js
+++ js/data/stat_data.js
@@ -1,6 +1,6 @@
-﻿var AT = "at", DF = "df", SA = "sa", SD = "sd", SP = "sp", SL = "sl";
+﻿var HP = "hp", AT = "at", DF = "df", SA = "sa", SD = "sd", SP = "sp", SL = "sl";
 var STATS_RBY = [AT, DF, SL, SP];
-var STATS_GSC = [AT, DF, SA, SD, SP];
+var STATS_GSC = [HP, AT, DF, SA, SD, SP];
 
 function CALC_HP_RBY(poke) {
 	var hp = poke.find(".hp");
