9c5d7dc97a905f59a3ae5b21035ae6216561a84e
Matt Washer <mjwasher@gmail.com>
2019-06-08 21:30:10 +1000

    nvm, missed something, should hopefully work now

--- js/ko_chance.js
+++ js/ko_chance.js
@@ -6,10 +6,10 @@
 		return 'aim for the horn next time';
 	}
 	//Code doesn't really work if these aren't set.
-	if (move.usedTimes === undefined){
+	if (move.usedTimes === undefined) {
 		move.usedTimes = 1;
 	}
-	if (move.metronomeCount === undefined){
+	if (move.metronomeCount === undefined) {
 		move.metronomeCount = 1;
 	}
 	if (damage[0] >= defender.maxHP && (move.usedTimes === 1 && move.metronomeCount === 1)) {
