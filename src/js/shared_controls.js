6f98aaa1b4a37014b3a2604fd0b90fb217109a3f
Kirk Scheibelhut <kjs@scheibo.com>
2019-08-11 19:36:15 +0200

    Add Google Analytics tracking code

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -690,6 +690,7 @@ var gen, genWasChanged, notation, pokedex, setdex, typeChart, moves, abilities, 
 $(".gen").change(function () {
 	/*eslint-disable */
 	gen = ~~$(this).val() || 7;
+	if ('ga' in window) ga('send', 'event', 'gen', gen);
 	genWasChanged = true;
 	/* eslint-enable */
 	// declaring these variables with var here makes z moves not work; TODO
