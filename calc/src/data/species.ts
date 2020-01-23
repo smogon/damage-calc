b93c4aef52f4219034196f50b05ab29e998be5d7
Austin Couturier <austincouturier@icloud.com>
2020-01-23 14:32:54 -0700

    Add Eternatus-Eternamax

--- calc/src/data/species.ts
+++ calc/src/data/species.ts
@@ -7939,6 +7939,16 @@ const SS: {[name: string]: Species} = extend(true, {}, SM, {
     w: 950,
     ab: 'Pressure',
     gender: 'genderless',
+    formes: ['Eternatus', 'Eternatus-Eternamax'],
+  },
+  'Eternatus-Eternamax': {
+    t1: 'Poison',
+    t2: 'Dragon',
+    bs: {hp: 225, at: 115, df: 250, sa: 125, sd: 250, sp: 130},
+    w: 950,
+    ab: 'Pressure',
+    gender: 'genderless',
+    isAlternateForme: true,
   },
   Falinks: {
     t1: 'Fighting',
