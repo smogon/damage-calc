fc9431070b021f67b3fa078448872abd2eb59834
Kirk Scheibelhut <kjs@scheibo.com>
2020-04-15 16:46:05 -0700

    Add support for running jest --coverage

--- calc/jest.config.js
+++ calc/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/' ],
   globals: {
     'ts-jest': {
       packageJson: 'package.json',
     },
   },
 };
+
