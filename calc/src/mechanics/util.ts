a063e3d4ee36a28ad2546b191a60f78bdc462656
Andrew Hyndman <ajhyndman@hotmail.com>
2020-07-27 10:46:04 -0700

    Fix Shell Side Arm's category (#404)
    Shell Side Arm's category should depend on relative damage, not just def

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -2,6 +2,7 @@ import {
   Generation,
   ID,
   ItemName,
+  MoveCategory,
   NatureName,
   StatName,
   StatsTable,
@@ -354,6 +355,27 @@ export function getFinalDamage(
   if (isBurned) damageAmount = Math.floor(damageAmount / 2);
   if (protect) damageAmount = pokeRound(OF32(damageAmount * 0x400) / 0x1000);
   return OF16(pokeRound(Math.max(1, OF32(damageAmount * finalMod) / 0x1000)));
+}
+
+/**
+ * Determines which move category Shell Side Arm should behave as.
+ *
+ * A simplified formula can be used here compared to what the research
+ * suggests as we do not want to implement the random tiebreak element of
+ * move - instead we simply default to 'Special' and allow the user to override
+ * this by manually adjusting the move's category.
+ *
+ * See also:
+ * {@link https://github.com/smogon/pokemon-showdown/commit/65d2bb5d}
+ *
+ * @param source Attacking pokemon (after stat modifications)
+ * @param target Target pokemon (after stat modifications)
+ * @returns 'Physical' | 'Special'
+ */
+export function getShellSideArmCategory(source: Pokemon, target: Pokemon): MoveCategory {
+  const physicalDamage = source.stats.atk / target.stats.def;
+  const specialDamage = source.stats.spa / target.stats.spd;
+  return physicalDamage > specialDamage ? 'Physical' : 'Special';
 }
 
 export function getWeightFactor(pokemon: Pokemon) {
