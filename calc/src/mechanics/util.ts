b706cf8f7a407bc1c052e85b95887be6729e15b7
Austin Couturier <austincouturier@icloud.com>
2020-09-20 09:29:27 -0600

    Fix weight based calculations with pokemon with Float Stone

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -379,7 +379,8 @@ export function getShellSideArmCategory(source: Pokemon, target: Pokemon): MoveC
 }
 
 export function getWeightFactor(pokemon: Pokemon) {
-  return pokemon.hasAbility('Heavy Metal') ? 2 : pokemon.hasAbility('Light Metal') ? 0.5 : 1;
+  return pokemon.hasAbility('Heavy Metal') ? 2
+  : (pokemon.hasAbility('Light Metal') || pokemon.hasItem('Float Stone')) ? 0.5 : 1;
 }
 
 export function countBoosts(gen: Generation, boosts: StatsTable) {
