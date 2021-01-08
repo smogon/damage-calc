15a9a70e9c2aca82577d0c42705e0f36522cf0be
Jonathan Arndt <jarndt@salesforce.com>
2021-01-08 00:50:42 -0700

    adding docker with server.js as calc api

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM node:12
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+
+RUN npm install
+
+WORKDIR /usr/src/app/calc
+COPY calc/package*.json ./
+RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+
+WORKDIR /usr/src/app
+COPY . .
+RUN node build
+
+EXPOSE 3000
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const calc = require("calc");
+const app = express();
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
+
+// parse application/json
+app.use(express.json())
+
+app.get("/calculate",(req, res, next) => {
+	const gen = calc.Generations.get((typeof req.body.gen === 'undefined') ? 8 : req.body.gen);
+	let error = "";
+	if(typeof req.body.attackingPokemon === 'undefined')
+		error += "attackingPokemon must exist and have a valid pokemon name\n";
+	if(typeof req.body.defendingPokemon === 'undefined')
+		error += "defendingPokemon must exist and have a valid pokemon name\n";
+	if(error)
+		throw new Error(error)
+	const result = calc.calculate(
+		gen,
+		new calc.Pokemon(gen, req.body.attackingPokemon, req.body.attackingPokemonOptions),
+		new calc.Pokemon(gen, req.body.defendingPokemon, req.body.defendingPokemonOptions),
+		new calc.Move(gen, req.body.moveName),
+		new calc.Field((typeof req.body.field === 'undefined') ? undefined : req.body.field)
+	);
+	res.json(result);
+})
+
+app.use(express.static('dist'))
