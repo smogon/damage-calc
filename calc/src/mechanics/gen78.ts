ac1bd2fbcfc275301ef8542196e9062019ea0a39
Leonard Craft III <leonardcraft64@gmail.com>
2021-05-10 21:45:09 -0500

    Clean up Gen 56

--- calc/src/mechanics/gen78.ts
+++ calc/src/mechanics/gen78.ts
@@ -692,7 +692,7 @@ export function calculateBasePowerSMSS(
     basePower = move.hits === 2 ? 30 : move.hits === 3 ? 40 : 20;
     desc.moveBP = basePower;
     break;
-    // Triple Kick's damage doubles after each consecutive hit (10, 20, 30), this is a hack
+  // Triple Kick's damage doubles after each consecutive hit (10, 20, 30), this is a hack
   case 'Triple Kick':
     basePower = move.hits === 2 ? 15 : move.hits === 3 ? 30 : 10;
     desc.moveBP = basePower;
