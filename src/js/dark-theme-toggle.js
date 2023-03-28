65e12dad1fce3a98d986fac6febddb69c6cb7ee8
shrianshChari <30420527+shrianshChari@users.noreply.github.com>
2023-03-27 23:20:47 -0400

    Dark Mode (#525)
    * Finished dark theme
    
    * Fixing code for tests; I miss es6
    
    * Reduced FOIT, but is still there
    
    * Added dark theme to Dynamax buttons
    
    * Removed duplicate classes
    
    * Added dark mode to Randoms calc
    
    * Removed unnecessary dark-theme class
    
    * Added Dark Mode for Honkalculator
    Has small styling issues that I couldn't figure out how to fix
    
    * Styled secret "Search" label

---
+++ src/js/dark-theme-toggle.js
@@ -0,0 +1,35 @@
+/*
+* Dark mode toggle
+*
+* In its current state, it will cause a minor FOIT.
+* Basically, the background behind the panels will
+* briefly flash white before turning dark. It's
+* better than before, but not perfect.
+*/
+
+/*
+* localStorage will only store strings
+* This means that if it has the value 'false',
+* It will be truey and incorrectly cause the
+* dark theme to load.
+*/
+var prefersDarkTheme = localStorage.getItem('darkTheme') === 'true';
+
+var darkThemeButton = document.getElementById('dark-theme-toggle');
+darkThemeButton.innerText = prefersDarkTheme ? 'Click for Light Theme' : 'Click for Dark Theme';
+
+/*
+* Function that toggles light and dark mode
+* Doesn't use jQuery, probably could with some modification
+*/
+function toggleTheme() {
+	prefersDarkTheme = !prefersDarkTheme;
+
+	var darkStyles = document.getElementById('dark-theme-styles');
+	darkStyles.disabled = !darkStyles.disabled;
+
+	localStorage.setItem('darkTheme', prefersDarkTheme);
+	darkThemeButton.innerText = prefersDarkTheme ? 'Click for Light Theme' : 'Click for Dark Theme';
+}
+
+darkThemeButton.addEventListener('click', toggleTheme);
