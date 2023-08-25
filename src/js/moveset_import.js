90024d1f5a70ef9da9e6b31d5bb7180d15a2655b
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2023-08-25 09:29:10 +0200

    Added a fix where the move nature power was overwritting the nature to '-'

--- src/js/moveset_import.js
+++ src/js/moveset_import.js
@@ -153,7 +153,7 @@ function getStats(currentPoke, rows, offset) {
 		}
 
 		currentNature = rows[x] ? rows[x].trim().split(" ") : '';
-		if (currentNature[1] == "Nature") {
+		if (currentNature[1] == "Nature" && currentNature[2] !== "Power") { // edge case with the move nature power that overwrite nature)
 			currentPoke.nature = currentNature[0];
 		}
 	}
