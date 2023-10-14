d079590392fe513a0f3dae18956a202da6fbe576
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-10-13 20:55:12 -0600

    Fix Gen 5 Randoms damage calc

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -577,7 +577,7 @@ $(".set-selector").change(function () {
 			$(this).closest('.poke-info').find(".extraSetAbilities").text(listAbilities.join(', '));
 			if (gen >= 2) $(this).closest('.poke-info').find(".item-pool").show();
 			$(this).closest('.poke-info').find(".extraSetItems").text(listItems.join(', '));
-			if (gen >= 9 || gen === 7 || gen === 6) {
+			if (gen >= 9 || gen === 7 || gen === 6 || gen === 5) {
 				$(this).closest('.poke-info').find(".role-pool").show();
 				if (gen >= 9) $(this).closest('.poke-info').find(".tera-type-pool").show();
 			}
@@ -631,7 +631,7 @@ $(".set-selector").change(function () {
 			}
 			var setMoves = set.moves;
 			if (randset) {
-				if (gen < 9 && gen !== 7 && gen !== 6) {
+				if (gen < 9 && gen !== 7 && gen !== 6 && gen !== 5) {
 					setMoves = randset.moves;
 				} else {
 					setMoves = [];
@@ -896,7 +896,7 @@ function createPokemon(pokeInfo) {
 			evs[stat] = (set.evs && typeof set.evs[legacyStat] !== "undefined") ? set.evs[legacyStat] : 0;
 		}
 		var moveNames = set.moves;
-		if (isRandoms && (gen >= 9 || gen === 7 || gen === 6)) {
+		if (isRandoms && (gen >= 9 || gen === 7 || gen === 6 || gen === 5)) {
 			moveNames = [];
 			for (var role in set.roles) {
 				for (var q = 0; q < set.roles[role].moves.length; q++) {
