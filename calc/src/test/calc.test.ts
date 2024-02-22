10fef28b1fd12589abe9c33e62fdcc2dab7b6767
Zachary Perlmutter <zrp200@gmail.com>
2024-02-22 14:34:27 -0800

    added associated tests

--- calc/src/test/calc.test.ts
+++ calc/src/test/calc.test.ts
@@ -990,6 +990,18 @@ describe('calc', () => {
         testQP('Protosynthesis', {weather: 'Sun'});
         testQPOverride('Quark Drive', {terrain: 'Electric'});
         testQPOverride('Protosynthesis', {weather: 'Sun'});
+        test('Meteor Beam/Electro Shot', () => {
+          // test only tests Electro Shot
+          const defender = Pokemon('Arceus');
+          const testCase = (options: object, expected: number) => {
+            const result = calculate(Pokemon('Archaludon', options), defender, Move('Electro Shot'));
+            expect(result.attacker.boosts.spa).toBe(expected);
+          };
+          testCase({}, 1); // raises by 1
+          testCase({boosts: {spa: 6}}, 6); // caps at +6
+          testCase({ability: 'Simple'}, 2);
+          testCase({ability: 'Contrary'}, -1);
+        });
         test('Revelation Dance should change type if Pokemon Terastallized', () => {
           const attacker = Pokemon('Oricorio-Pom-Pom');
           const defender = Pokemon('Sandaconda');
