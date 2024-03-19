d2fd146c05b5849346787c29c5dfa1bcca8ef870
placuszek15 <47085922+placuszek15@users.noreply.github.com>
2024-03-19 06:38:29 +0100

    Update src/js/oms_controls.js
    add brackets
    
    Co-authored-by: Waleed Hassan <waleed.hassan.aidid@gmail.com>

--- src/js/oms_controls.js
+++ src/js/oms_controls.js
@@ -168,15 +168,10 @@ function autoUpdateStats(p) {
 		}
 	}
 	if (shouldUseTS) {
-		if (gen >= 9) {
-			var conversion = CONVERSION_G9;
-		} else {
-			var conversion = CONVERSION_PREG9;
-		}
 		// have to do it inside function cause of timing, has to be after gen is set
 		// have a backup 0 just in case the tier doesn't exist ex: somehow failed to fetch
 		var tier = tierBlob[toID(pokemon)] || '';
-		var tierAddon = conversion[tier.toLowerCase()] || 0;
+		var tierAddon = (gen >= 9 ? CONVERSION_G9 : CONVERSION_PREG9)[tier.toLowerCase()] || 0;
 		for (var i = 1; i < LEGACY_STATS[gen].length; i++) {
 			var stat = LEGACY_STATS[gen][i];
 			volatileCopy.bs[stat] = clampStats(volatileCopy.bs[stat] + tierAddon);
