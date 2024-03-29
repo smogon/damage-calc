eee794eba7d5b80457a79a705082c6257dd917e6
Vertigo <vertigo06ps@gmail.com>
2024-03-29 02:51:02 +0100

    Accuracy-dependent multihit moves should not get BP boost from Tera

--- calc/src/mechanics/gen789.ts
+++ calc/src/mechanics/gen789.ts
@@ -926,7 +926,7 @@ export function calculateBasePowerSMSSSV(
   basePower = OF16(Math.max(1, pokeRound((basePower * chainMods(bpMods, 41, 2097152)) / 4096)));
   if (
     attacker.teraType && move.type === attacker.teraType &&
-    attacker.hasType(attacker.teraType) && move.hits === 1 &&
+    attacker.hasType(attacker.teraType) && move.hits === 1 && !move.multiaccuracy &&
     move.priority <= 0 && move.bp > 0 && !move.named('Dragon Energy', 'Eruption', 'Water Spout') &&
     basePower < 60 && gen.num >= 9
   ) {
