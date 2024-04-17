01d1fe75dd983096b5635400826111b3bf1cc8cc
singiamtel <47090312+singiamtel@users.noreply.github.com>
2024-04-17 02:10:49 +0200

    Fix auto selecting Dynamax moves (#619)
    bisect: 01467a88441440937f6f19d28c95b09c86c5518c
    
    Bug was an oversight while refactoring

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -1106,7 +1106,7 @@ function getMoveDetails(moveInfo, opts) {
 	return new calc.Move(gen, moveName, {
 		ability: opts.ability, item: opts.item, useZ: isZMove, species: opts.species, isCrit: isCrit, hits: hits,
 		isStellarFirstUse: isStellarFirstUse, timesUsed: timesUsed, timesUsedWithMetronome: timesUsedWithMetronome,
-		overrides: overrides, useMax: opts.useMax
+		overrides: overrides, useMax: opts.isDynamaxed
 	});
 }
 
