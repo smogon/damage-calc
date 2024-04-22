836da4382bc0bfc659fcf1695b0507b24e7441cd
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-04-22 13:46:28 -0400

    Remove .holder-0 CSS and make it an ID

--- src/js/honkalculate_controls.js
+++ src/js/honkalculate_controls.js
@@ -342,10 +342,10 @@ function calcDTDimensions() {
 	});
 
 	var theadBottomOffset = getBottomOffset($(".sorting"));
-	var heightUnderDT = getBottomOffset($(".holder-0")) - getBottomOffset($("#holder-2 tbody"));
+	var heightUnderDT = getBottomOffset($("#holder-0")) - getBottomOffset($("#holder-2 tbody"));
 	dtHeight = $(document).height() - theadBottomOffset - heightUnderDT;
 	dtWidth = $(window).width() - $("#holder-2").offset().left;
-	dtWidth -= 2 * parseFloat($(".holder-0").css("padding-right"));
+	dtWidth -= 2 * parseFloat($("#holder-0").css("padding-right"));
 }
 
 function getBottomOffset(obj) {
