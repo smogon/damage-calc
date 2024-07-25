b2beae431bee57453604bb6e670ab74905ed0ce3
Arley Pádua <17691875+arleypadua@users.noreply.github.com>
2024-07-25 22:44:38 +0100

    UI: Allow querystring set imports

--- src/js/index_randoms_controls.js
+++ src/js/index_randoms_controls.js
@@ -199,6 +199,17 @@ $(document).ready(function () {
 			}
 		}
 	}
+
+	var importParam = params.get('import');
+	if (importParam) {
+		try {
+			var decodedImport = atob(importParam); // Decode base64
+			$('.import-team-text').val(decodedImport); // Set value to text area
+		} catch (e) {
+			console.error('Failed to decode Import parameter:', e);
+		}
+	}
+
 	$(".calc-trigger").bind("change keyup", PC_HANDLER);
 	performCalculations();
 });
