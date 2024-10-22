96a1e83a0e0b7d7927ab383efe9eace7fb891f35
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-10-22 10:59:49 -0400

    Config syntax more closely matches jest examples

--- calc/jest.config.js
+++ calc/jest.config.js
@@ -5,11 +5,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/' ],
   transform: {
-    '^.+\\.test\\.ts$': [
-      'ts-jest', {
-        packageJson: 'package.json',
-      }
-    ],
+    '^.+\\.test\\.ts$': 'ts-jest',
   },
 };
 
