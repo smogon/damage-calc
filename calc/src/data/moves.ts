33fe0c5c35e3f93e72e97989e9f8bb935de44b91
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-10-26 14:45:57 -0400

    Jet Punch is no longer Sheer Force boosted

--- calc/src/data/moves.ts
+++ calc/src/data/moves.ts
@@ -4482,7 +4482,6 @@ const SV_PATCH: {[name: string]: DeepPartial<MoveData>} = {
     makesContact: true,
     isPunch: true,
     priority: 1,
-    // Sheer Force boost implemented in gen789.ts
   },
   'Kowtow Cleave': {
     bp: 85,
--- calc/src/mechanics/gen789.ts
+++ calc/src/mechanics/gen789.ts
@@ -1176,7 +1176,7 @@ export function calculateBPModsSMSSSV(
   // Sheer Force does not power up max moves or remove the effects (SadisticMystic)
   if (
     (attacker.hasAbility('Sheer Force') &&
-      (move.secondaries || move.named('Jet Punch', 'Order Up')) && !move.isMax) ||
+      (move.secondaries || move.named('Order Up')) && !move.isMax) ||
     (attacker.hasAbility('Sand Force') &&
       field.hasWeather('Sand') && move.hasType('Rock', 'Ground', 'Steel')) ||
     (attacker.hasAbility('Analytic') &&
