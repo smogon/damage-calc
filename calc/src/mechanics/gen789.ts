28efeb28850666fafcaa5774906c1bb6687d31a0
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-11-02 23:24:45 -0400

    Physical Shell Side Arm should make contact

--- calc/src/mechanics/gen789.ts
+++ calc/src/mechanics/gen789.ts
@@ -134,6 +134,11 @@ export function calculateSMSSSV(
     move.flags.contact = 0;
   }
 
+  if (move.named('Shell Side Arm') &&
+    getShellSideArmCategory(attacker, defender) === 'Physical') {
+    move.flags.contact = 1;
+  }
+
   const breaksProtect = move.breaksProtect || move.isZ || attacker.isDynamaxed ||
   (attacker.hasAbility('Unseen Fist') && move.flags.contact);
 
