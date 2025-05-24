2c09b539c64c8f1ddf94891122516d1cc6709686
Waleed Hassan <waleed.hassan.aidid@gmail.com>
2025-05-24 22:38:13 +0300

    Import Reg I

--- import/src/set-import.ts
+++ import/src/set-import.ts
@@ -177,11 +177,11 @@ function getSpecie(gen: Generation, specieName: SpeciesName): Specie | PSSpecie 
 }
 
 function toPSFormat(formatID: ID): ID {
-  if (formatID === 'gen9vgc2024') {
-    return `gen9vgc2025regg` as ID;
+  if (formatID === 'gen9vgc2025') {
+    return `gen9vgc2025regi` as ID;
   }
   if (formatID === 'gen9battlestadiumsingles') {
-    return 'gen9bssregg' as ID;
+    return 'gen9bssregi' as ID;
   }
   return formatID;
 }
