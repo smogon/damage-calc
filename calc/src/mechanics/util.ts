46bd3c4632c9453d5f51735885aecca084d61be6
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2026-04-17 23:49:05 -0700

    Champions: Paralysis should halve speed

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -128,7 +128,8 @@ export function getFinalSpeed(gen: Generation, pokemon: Pokemon, field: Field, s
 
   speed = OF32(pokeRound((speed * chainMods(speedMods, 410, 131172)) / 4096));
   if (pokemon.hasStatus('par') && !pokemon.hasAbility('Quick Feet')) {
-    speed = Math.floor(OF32(speed * (gen.num < 7 ? 25 : 50)) / 100);
+    // gen.num = 0 means it's Pokemon Champions
+    speed = Math.floor(OF32(speed * (gen.num >= 7 || gen.num == 0 ? 50 : 25)) / 100);
   }
 
   speed = Math.min(gen.num <= 2 ? 999 : 10000, speed);
